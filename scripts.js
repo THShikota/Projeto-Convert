43994873998b4772691126ebf91e61d901a857c1
Tiago Shikota <th.shikota@gmail.com>
2025-11-02 18:35:02 -0300

    Criação de cotações de moeda e script para identificar a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+//Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -6,12 +11,27 @@ const currency = document.getElementById("currency")
 // Manipulando o input amount parar receber somente números.
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g
-    amount.value  = amount.value.replace(hasCharactersRegex, "")
+    amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
 // Captando o evento de submit do formulário.
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log(currency.value)
+    switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol){
+  console.log(amount, price, symbol)
 }
